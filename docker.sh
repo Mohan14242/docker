ec710b3a9a7623ed344f4720c1fd6959df8e0e9d
mohan <mohan14242@gmail.com>
2024-01-21 12:29:19 +0530

    mohan

--- docker.sh
+++ docker.sh
@@ -3,3 +3,4 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker centos
